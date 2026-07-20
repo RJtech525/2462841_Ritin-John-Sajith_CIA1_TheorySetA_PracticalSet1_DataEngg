3025734e71f09d31e31dd1cacd0513349ed86b8a
RJtech525 <johnritin525@gmail.com>
2026-07-20 11:45:28 +0530

    Add multiple student entries to the database
    Inserted multiple student records into the database.

---
+++ Set1/Task8.sql
@@ -0,0 +1,6 @@
+-- Task 8: College Student Database — Insert Multiple Rows
+INSERT INTO students (Student_ID, Name, Course, Marks, City, Email, Remarks)
+VALUES 
+    (102, 'Arya', 'CS', 98.00, 'Bangalore', 'arya@gmail.com', 'Top scorer'),
+    (103, 'Ivin', 'ECE', 89.00, 'Salem', 'ivin@gmail.com', 'Good progress'),
+    (104, 'Samuel', 'Mech', 90.00, 'Andhra', 'samuel@gmail.com', 'sports achiever');
